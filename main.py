beda15684e8854e1d22afbf3457e1ae394b38d04
Alexander Morales <amoralesmm@yahoo.com>
2024-05-05 18:45:21 -0500

    Rock Paper Scissors Game completed.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import rockpaperandscissors
+import random
 
-print(rockpaperandscissors.random_num)
-print(rockpaperandscissors.random_float)
+# print(rockpaperandscissors.random_num)
+# print(rockpaperandscissors.random_float)
+
+player = rockpaperandscissors.get_player_choice()
+computer = rockpaperandscissors.get_computer_choice()
+result = rockpaperandscissors.winner(player, computer)
+
+print("You choose " + player + "!")
+print("Computer choose " + computer + "!")
+print(result)
--- rockpaperandscissors.py
+++ rockpaperandscissors.py
@@ -1,12 +1,41 @@
 import random
 
-random_num = random.randint(1,10)
-random_float = random.random() * 10
+### Winning Conditions
+def winner(player_choice, computer_choice):
+    winning_conditions = {
+        ('rock', 'scissors'): 'player',
+        ('paper', 'rock'): 'player',
+        ('scissors', 'paper'): 'player'
+    }
+    if player_choice == computer_choice:
+        return 'Tie!'
+    if (player_choice, computer_choice) in winning_conditions:
+        return 'Congratulations you won!'
+    else:
+        return 'You lost!'
+    
+def get_player_choice():
+    ### Player Chooses    
+    print("Rock Paper Scissors!")
+    player = input('Type your choice! Rock, Paper or Scissors! ').lower()
+
+    ### Validate user input
+    while player not in ['rock', 'paper', 'scissors']:
+        print("Invalid selection, Please type Rock, Paper or Scissors")
+        player = input('Type your choice! Rock, Paper or Scissors! ').lower()
+    
+    return player
+    
+def get_computer_choice():
+    ### Computer Chooses
+    computer_choices = ['rock', 'paper', 'scissors']
+    computer = random.choice(computer_choices)
+    
+    return computer
 
 
-print("Select your weapon")
-player = input("1. Rock\n2. Paper\n3. Scissors\n")
-computer = random.randint(0,2)
 
-print(f"You selected " + player + "!")
-print(computer)
+
+### Notes
+# random_num = random.randint(1,10)
+# random_float = random.random() * 10
