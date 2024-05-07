1a1fdc0eb6d9b1c3d18af0649bf6069bd0cab290
Alexander Morales <amoralesmm@yahoo.com>
2024-05-06 23:29:46 -0500

    Clean up code

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-
 # Define lowercase letters, uppercase letters, digits, and symbols
 all_letters = list(string.ascii_lowercase + string.ascii_uppercase)
 digits = list(string.digits)
@@ -10,7 +9,6 @@ symbols = list("!@#$%^&*()-_=+[]{}|;:,.<>?/~")
 # letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 # numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 # symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
 
 print("Password Generator")
 
@@ -31,7 +29,7 @@ for char in range(1, num_symbols +1):
     random_char = random.choice(symbols)
     password += random_char
 #print(password)
-
+    
 characters = list(password)
 random.shuffle(characters)
 new_password = ''.join(characters)
